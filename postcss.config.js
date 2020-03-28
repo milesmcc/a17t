13998774e8ced9615ebb70e33cbcf093358b3e7f
R. Miles McCain <x1-key@sendmiles.email>
2020-03-27 22:44:13 -0400

    Progress! On a more sane version

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-each"),
+    require("postcss-preset-env")({
+      stage: 4
+    }),
+    require('postcss-custom-properties'),
+    require('tailwindcss'),
+    require("cssnano")({
+      autoprefixer: false
+    })
+  ]
+};
