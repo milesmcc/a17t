a57058535fd74361fad8a428023c7a89583bcc92
R. Miles McCain <github@sendmiles.email>
2021-12-21 20:26:01 -0500

    Doc updates

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,7 +1,18 @@
+let colors = require("tailwindcss/colors")
+
 module.exports = {
     content: ["./**/*.{html,md}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                neutral: colors.gray,
+                positive: colors.green,
+                urge: colors.purple,
+                warning: colors.yellow,
+                info: colors.blue,
+                error: colors.red
+            }
+        },
     },
     plugins: [
         require("../src/a17t.js")
