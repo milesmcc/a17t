6431509f0809fb8ca8341c56733959528388d421
R. Miles McCain <github@sendmiles.email>
2021-12-21 22:11:23 -0500

    Migrate subheading

---
+++ src/typography/subheading.js
@@ -0,0 +1,9 @@
+module.exports = ({ theme }) => {
+    return {
+        ".subheading": {
+            "color": "var(--color-content)",
+            "opacity": "0.8",
+            "font-size": theme("fontSize.xl"),
+        }
+    }
+}
