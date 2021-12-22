4a468e5851744dcdff3a177c47bb2419ac3c7649
R. Miles McCain <github@sendmiles.email>
2021-12-21 19:12:12 -0500

    Migrate aside

---
+++ src/layout/aside.js
@@ -0,0 +1,13 @@
+module.exports = ({ theme }) => {
+  return {
+      ".\\@aside": {
+          "border-left": "3px solid var(--color-content)",
+          "background-color": "var(--color-fill)",
+          "color": "var(--color-content)",
+          "display": "block",
+          "padding": theme("spacing.4"),
+          "overflow": "hidden",
+          "border-radius": theme("borderRadius.lg")
+      }
+  }
+}
