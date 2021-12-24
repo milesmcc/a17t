eb1b360c717f9427adc98aee1ccc20461ff79bd3
R. Miles McCain <github@sendmiles.email>
2021-12-23 20:04:05 -0500

    Remove color and size from heading

--- src/typography/heading.js
+++ src/typography/heading.js
@@ -1,8 +1,6 @@
 module.exports = ({ theme }) => {
     return {
         ".heading": {
-            "color": "var(--color-content)",
-            "font-size": theme("fontSize.3xl"),
             "font-weight": theme("fontWeight.semibold"),
             "line-height": theme("lineHeight.tight")
         }
