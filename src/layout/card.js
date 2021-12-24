d1be2a9a7aedbf2f9aff7aa027b4cdf49a7fd064
R. Miles McCain <github@sendmiles.email>
2021-12-23 21:58:56 -0500

    Remove block, add border to card

--- src/layout/card.js
+++ src/layout/card.js
@@ -1,12 +1,12 @@
 module.exports = ({ theme }) => {
     return {
         ".card": {
-            "display": "block",
             "padding": theme("spacing.4"),
             "background-color": theme("colors.bg"),
             "overflow": "hidden",
             "border-radius": theme("borderRadius.lg"),
             "box-shadow": theme("boxShadow.lg"),
+            "border": "1px solid " + theme("colors.gray.100"),
         }
     }
 }
