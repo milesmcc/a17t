d4b38edba9afa9c61f91df29eed7b892200e6647
R. Miles McCain <github@sendmiles.email>
2021-12-23 19:54:37 -0500

    Migrate field

---
+++ src/interaction/field.js
@@ -0,0 +1,37 @@
+module.exports = ({ theme }) => {
+  return {
+      ".field, .input": {
+          "transition": "box-shadow 75ms ease-out, border 75ms ease-out",
+          "box-shadow": theme("boxShadow.sm"),
+          "appearance": "none",
+          "background-color": "inherit",
+          "-webkit-appearance": "none",
+          "-moz-appearance": "none",
+          "&[disabled]": {
+            "cursor": "not-allowed",
+            "opacity": "0.6",
+          },
+          "&[readonly]": {
+            "user-select": "all",
+          },
+          "&::placeholder": {
+            "opacity": "0.75",
+          },
+          "&:hover": {
+            "box-shadow": "0 0 0 " + theme("borderWidth")["1.5"] + " var(--color-fill), " + theme("boxShadow.sm"),
+          },
+          "&:focus": {
+            "box-shadow": "0 0 0 " + theme("borderWidth.2") + " var(--color-fill), " + theme("boxShadow.sm"),
+            "outline": "none"
+          },
+          "display": "inline-flex",
+          "width": "100%",
+          "max-width": "100%",
+          "padding": theme("spacing.2"),
+          "line-height": theme("lineHeight.tight"),
+          "border": theme("borderWidth.DEFAULT") + " solid var(--color-fill)",
+          "border-radius": theme("borderRadius.DEFAULT"),
+          "background-color": theme("colors.bg")
+      }
+  }
+}
