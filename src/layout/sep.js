c8bf259ca2a8d9e2d87a72dd5c929448befbe731
R. Miles McCain <github@sendmiles.email>
2021-12-25 14:45:00 -0500

    Fix sep spacing to match old versions

--- src/layout/sep.js
+++ src/layout/sep.js
@@ -2,7 +2,7 @@ module.exports = ({ theme }) => {
     return {
         ".sep": {
             "background-color": theme("colors.transparent"),
-            "height": theme("spacing.8"),
+            "height": theme("spacing.12"),
             "border": "none"
         }
     }
