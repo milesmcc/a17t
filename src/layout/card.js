5b3c958f9cce9077520f5260be159be6a6bddf27
R. Miles McCain <github@sendmiles.email>
2021-12-27 11:36:26 -0500

    Refresh .card (add tone and priority support)

--- src/layout/card.js
+++ src/layout/card.js
@@ -4,10 +4,14 @@ module.exports = ({ theme }) => {
             "display": "block",
             "padding": theme("spacing.4"),
             "background-color": theme("colors.bg"),
+            "border": "1px solid var(--color-accent, " + theme("colors.gray.100") + ")",
+            "&.\\@low, &.\\@high": {
+                "background-color": "var(--color-fill)",
+                "color": "var(--color-content)",
+            },
             "overflow": "hidden",
             "border-radius": theme("borderRadius.lg"),
             "box-shadow": theme("boxShadow.md"),
-            "border": "1px solid " + theme("colors.gray.100"),
         }
     }
 }
